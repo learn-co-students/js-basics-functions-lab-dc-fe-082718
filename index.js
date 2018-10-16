9c6a46242b44fcb6ea35cf2dec2ad38a4a21432e
Sam Birk <sam.birk@flatironschool.com>
2018-10-16 00:39:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+  return  Math.abs(42 - blockNumber)
+}
+
+function distanceFromHqInFeet(blockNumber) {
+  return distanceTravelledInFeet(blockNumber, 42);
+}
+
+function distanceTravelledInFeet(start, end) {
+  numberOfBlocks =  Math.abs(end - start);
+  return numberOfBlocks * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  feet = distanceTravelledInFeet(start, destination)
+  if(feet < 400) {
+    return 0;
+  } else if (feet >= 400 && feet < 2000) {
+    return (feet - 400) * .02;
+  } else if(feet > 2000 && feet <= 2500) {
+    return 25;
+  }
+  else {
+    return 'cannot travel that far'
+  }
+}
+
+document.getElemen
